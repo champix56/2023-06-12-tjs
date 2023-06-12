3dfc657f59c71f408f3ab1bf12530d7f4a697b3a
champix56 <desorbaix@free.fr>
2023-06-12 17:53:50 +0200

    gestion retour etat initial

--- src/App/components/ui/Button/Button.jsx
+++ src/App/components/ui/Button/Button.jsx
@@ -1,15 +1,23 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import style from './Button.module.css'
 import PropTypes from 'prop-types'
 const Button = (props) => {
-    console.log(props);
+    const [isClicked, setIsClicked] = useState(false)
+    useEffect(() => {
+        let to=undefined;
+        if (isClicked) {
+            setTimeout(() => {setIsClicked(false) }, 400)
+        }
+        return ()=>{
+            clearTimeout(to)
+        }
+    }, [isClicked])
     return (
         <button
             type={props.type}
-            className={`${style.Button} btn${
-                undefined!==props.className?' '+props.className:''
-            }`}
+            className={`${style.Button} btn${undefined !== props.className ? ' ' + props.className : ''}${true === isClicked ? ' ' + style.clicked : ''}`}
             onClick={(evt) => {
+                setIsClicked(true);
                 if (undefined !== props.onClick && typeof props.onClick === 'function') {
                     props.onClick('click');
                 }
@@ -23,7 +31,7 @@ Button.propTypes = {
     type: PropTypes.oneOf(['button', 'reset', 'submit']).isRequired,
     bgColor: PropTypes.string.isRequired,
     onClick: PropTypes.func,
-    className: PropTypes.oneOf(['primary','error'])
+    className: PropTypes.oneOf(['primary', 'error'])
 }
 Button.defaultProps = {
     type: 'button',
