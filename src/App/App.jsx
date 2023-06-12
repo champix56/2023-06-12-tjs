c24f6fd32cec4d63f8beac9dfab3dd2d51929f8b
champix56 <desorbaix@free.fr>
2023-06-12 12:17:06 +0200

    restructuration des structures folder des cmps

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import Button from '../Button/Button'
+import Button from './components/ui/Button/Button'
 function App() {
   return (
     <div className="App">
