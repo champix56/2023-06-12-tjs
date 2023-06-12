c5d6d3188d833c2af353c616818b3ebf7b8ff21c
champix56 <desorbaix@free.fr>
2023-06-12 12:09:52 +0200

    creation d'un button

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
-
+import Button from '../Button/Button'
 function App() {
   return (
     <div className="App">
-      DEMAT breizh
+      <Button></Button>
+      <Button></Button>
     </div>
   );
 }
---
+++ src/Button/Button.jsx
@@ -0,0 +1,6 @@
+import React from 'react'
+
+const Button=(props)=>{
+    return <button>Bejamin</button>;
+}
+export default Button
