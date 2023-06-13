81395f2545948bb827f12c4bd5ba512b23f299f1
champix56 <desorbaix@free.fr>
2023-06-13 14:00:07 +0200

    fin adapt html -> reactjsx

--- src/App/components/functional/MemeForm/MemeForm.js
+++ src/App/components/functional/MemeForm/MemeForm.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
-import styles from './MemeForm.module.css';
+import style from './MemeForm.module.css';
 export const initialStateMemeForm = {}
 const MemeForm = (props) => {
   const [state, setstate] = useState(initialStateMemeForm)
@@ -13,11 +13,11 @@ const MemeForm = (props) => {
     };
   }, [])
   return (
-    <div className={styles.MemeForm} data-testid="MemeForm">
+    <div className={style.MemeForm} data-testid="MemeForm">
       <form><label htmlFor="titre">
         <h1>Titre</h1></label>
         <br />
-        <input name="titre" id="titre" value="React is easy" />
+        <input name="titre" id="titre"  />
         <hr />
         <label htmlFor="image">
           <h2>Image</h2>
@@ -31,32 +31,32 @@ const MemeForm = (props) => {
           <h2>texte</h2>
         </label>
         <br />
-        <input name="text" id="text" type="text" value="Le js m'a tué" />
+        <input name="text" id="text" type="text"  />
         <br />
         <label htmlFor="x">
           <h2 >x :</h2>
         </label>
-        <input className={style.smallNumber} name="x" id="x" type="number" value="121" />
+        <input className={style.smallNumber} name="x" id="x" type="number"  />
         <label htmlFor="y">
           <h2 >y :</h2>
         </label>
-        <input className={style.smallNumber} name="y" id="y" type="number" value="65" />
+        <input className={style.smallNumber} name="y" id="y" type="number"  />
         <hr />
         <br />
-        <h2>Decorations</h2>
+        <h3>Decorations</h3>
         <label htmlFor="color">
           <h2 >color :</h2>
         </label>
-        <input name="color" id="color" type="color" value="#FFFFFF" />
+        <input name="color" id="color" type="color"  />
         <br />
         <label htmlFor="fontSize">
           <h2 >font-size :</h2>
         </label>
-        <input className={style.smallNumber} name="fontSize" id="fontSize" type="number" min="0" value="73" />px<br />
+        <input className={style.smallNumber} name="fontSize" id="fontSize" type="number" min="0"  />px<br />
         <label htmlFor="fontWeight">
           <h2 >font-weight :</h2>
         </label>
-        <input className={style.smallNumber} name="fontWeight" id="fontWeight" type="number" min="100" step="100" max="900" value="900" />
+        <input className={style.smallNumber} name="fontWeight" id="fontWeight" type="number" min="100" step="100" max="900"  />
         <br />
         <input name="underine" id="underline" type="checkbox" />
         &nbsp;
