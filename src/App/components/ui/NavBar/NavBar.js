9e2f5484804bac9e75b20ca0a7594bce52813ab2
champix56 <desorbaix@free.fr>
2023-06-13 12:24:12 +0200

    ajout de navbar

--- src/App/components/ui/NavBar/NavBar.js
+++ src/App/components/ui/NavBar/NavBar.js
@@ -1,10 +1,23 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import style from './NavBar.module.css'
+import '../../../../../node_modules/bootstrap/dist/css/bootstrap.css'
+// facultatif si non usage des applications js de bootstrap
+// attention pour le js pensez a yarn add jquery et import './..../dist/jquery.js'
+// import '../../../../../node_modules/bootstrap/dist/js/bootstrap.js'
+import { Navbar, Container, Nav } from 'react-bootstrap'
 const NavBar = (props) => {
   return (
     <div className={style.NavBar} data-testid="NavBar">
-      NavBar
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">Home</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Thumbnail</Nav.Link>
+            <Nav.Link href="#features">New mem</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
     </div>
   )
 }
