07995106c0d3ab15a8127ad88a7b689d6e2ff6d7
champix56 <desorbaix@free.fr>
2023-06-14 16:14:46 +0200

    ajout de link

--- src/App/components/ui/NavBar/NavBar.js
+++ src/App/components/ui/NavBar/NavBar.js
@@ -6,15 +6,28 @@ import '../../../../../node_modules/bootstrap/dist/css/bootstrap.css'
 // attention pour le js pensez a yarn add jquery et import './..../dist/jquery.js'
 // import '../../../../../node_modules/bootstrap/dist/js/bootstrap.js'
 import { Navbar, Container, Nav } from 'react-bootstrap'
+import { LinkContainer } from 'react-router-bootstrap'
 const NavBar = (props) => {
   return (
     <div className={style.NavBar} data-testid="NavBar">
       <Navbar bg="dark" variant="dark">
         <Container>
-          <Navbar.Brand href="#home">Home</Navbar.Brand>
+          <LinkContainer to="/">
+            <Navbar.Brand href="/">Home</Navbar.Brand>
+          </LinkContainer>
           <Nav className="me-auto">
-            <Nav.Link href="#home">Thumbnail</Nav.Link>
-            <Nav.Link href="#features">New mem</Nav.Link>
+            {/* <Link to="/thumbnail">Thumbnail</Link>
+            <Link to="/editor">new</Link>
+            <Link to="/editor/1">editor id1</Link> */}
+            <LinkContainer to="/thumbnail">
+              <Nav.Link href="/thumbnail">Thumbnail</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/meme">
+              <Nav.Link href="/meme">new</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/meme/1">
+              <Nav.Link href="/meme/1">Edit id:1</Nav.Link>
+            </LinkContainer>
           </Nav>
         </Container>
       </Navbar>
