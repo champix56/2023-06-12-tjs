99628e5ca31adc808e9764f864644b861f4d8d6e
champix56 <desorbaix@free.fr>
2023-06-14 12:05:09 +0200

    gestion async save

--- src/App/store/currentSlice.js
+++ src/App/store/currentSlice.js
@@ -1,5 +1,6 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
 import { emptyMeme } from 'orsys-tjs-meme'
+import { REST_ADR, ressourcesURI } from '../config/config';
 const initialState = emptyMeme
 
 const currentSlice = createSlice({
@@ -9,12 +10,31 @@ const currentSlice = createSlice({
         update: (state, action) => {
             Object.assign(state, action.payload)
         },
-        clear: (state) => { 
-            Object.assign(state, emptyMeme) 
+        clear: (state) => {
+            Object.assign(state, emptyMeme)
         }
+    },
+    extraReducers:(builder)=>{
+        builder.addCase('current/save/fulfilled',(state,action)=>{
+            Object.assign(state,action.payload)
+        })
     }
 });
 
 export const { update, clear } = currentSlice.actions
 // update(unMeme) -> {type:'current/update', payload:unMeme} 
+export const saveCurrent = createAsyncThunk('current/save', async (meme) => {
+   console.log(`saveCurren`,meme);
+    const promiseCurrent = await fetch(
+        `${REST_ADR}${ressourcesURI.memes}${undefined !== meme.id ? '/' + meme.id : ''}`,
+        {
+            method:undefined!==meme.id?'PUT':'POST',
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(meme)
+        })
+        return await promiseCurrent.json()
+
+})
 export default currentSlice.reducer
