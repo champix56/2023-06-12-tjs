a5a8b042ff56001263eb9cbb542a19ef082c60c3
champix56 <desorbaix@free.fr>
2023-06-14 10:56:10 +0200

    appel async

--- src/App/store/ressourcesSlice.js
+++ src/App/store/ressourcesSlice.js
@@ -1,16 +1,22 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
 
 const initialState = {
-    images:[],
-    memes:[]
+    images: [],
+    memes: []
 }
 
 const ressourcesSlice = createSlice({
-  name: 'ressources',
-  initialState,
-  reducers: {}
+    name: 'ressources',
+    initialState,
+    reducers: {}
 });
 
 // export const {} = ressourcesSlice.actions
-
+export const fetchAllRessources = createAsyncThunk('ressources/fetchRessources',
+    async () => {
+        const promiseImages = await fetch('http://localhost:5629/images')
+        const jso= await promiseImages.json() 
+        console.log(jso)
+        return jso
+    })
 export default ressourcesSlice.reducer
