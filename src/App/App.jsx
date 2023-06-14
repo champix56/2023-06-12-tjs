7cdfcddd968e0402719e6af72fd74a616ba51bd1
champix56 <desorbaix@free.fr>
2023-06-14 16:15:00 +0200

    construction conditionnel en fonction de la route

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,30 +1,37 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import FlexV3Grow from './components/layout/FlexV3Grow/FlexV3Grow'
 import Header from './components/ui/Header/Header'
 import NavBar from './components/ui/NavBar/NavBar'
 import FlexH1Grow from './components/layout/FlexH1Grow/FlexH1Grow'
 import Footer from './components/ui/Footer/Footer'
-import {MemeSVGViewer, emptyMeme} from 'orsys-tjs-meme'
-import MemeForm, { MemeFormStoredConnected } from './components/functional/MemeForm/MemeForm'
-import datas from './db.json'
-import store from './store/store'
+import { MemeFormStoredConnected } from './components/functional/MemeForm/MemeForm'
+import {
+  Route,
+  Routes,
+  useParams,
+  useLocation,
+} from 'react-router-dom'
+
 import MemeSvgViewer from './components/ui/MemeSvgViewer/MemeSvgViewer'
- 
+import Editor from './pages/editor'
+
 const App = () => {
- 
-//chargement de datas post 1er montage (fetch si besoin) 
-useEffect(() => {
- // store.dispatch(fetchAllRessources())
-}, [])
+  //chargement de datas post 1er montage (fetch si besoin) 
+  useEffect(() => {
+    // store.dispatch(fetchAllRessources())
+  }, [])
+  console.log(useParams(),useLocation());
   return (
     <div className="App">
       <FlexV3Grow>
         <Header />
         <NavBar />
-        <FlexH1Grow>
-          <MemeSvgViewer basePath=''/>
-          <MemeFormStoredConnected />
-        </FlexH1Grow>
+        <Routes>
+          <Route path='/' element={<div><h1>Hello a tous</h1></div>} />
+          <Route path='/thumbnail' element={<div>thumbnail</div>} />
+          <Route path='/meme' element={<Editor/>} />
+          <Route path='/meme/:id' element={<Editor/>} />
+        </Routes>
         <Footer />
       </FlexV3Grow>
     </div>
