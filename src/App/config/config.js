d2931a3c16f99c2f9a0d92d9517b36ed760dce3a
champix56 <desorbaix@free.fr>
2023-06-14 11:00:27 +0200

    fichier de constante de config

---
+++ src/App/config/config.js
@@ -0,0 +1,11 @@
+/**
+ * base d'url du serveur REST
+ */
+export const REST_ADR="http://localhost:5629"
+/**
+ * uri des ressources en fonction du nom de ressource
+ */
+export const ressourcesURI=Object.freeze({
+    memes:'/memes',
+    images:'/images'
+})
